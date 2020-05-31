f659aee12b3eff85104317b37f94c2f02306e3e2
hackbluecorp <62452915+hackbluecorp@users.noreply.github.com>
2020-05-30 20:23:52 -0500

    Updates

---
+++ precache-manifest.ae65af562c97208eed1472deeedfb115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca00abdfeb208b761cc5a3c1e068f43",
+    "url": "/reactagenda/index.html"
+  },
+  {
+    "revision": "7d911996e2f8324a224d",
+    "url": "/reactagenda/static/css/2.6d92adba.chunk.css"
+  },
+  {
+    "revision": "26b4ae337b3f3cba56de",
+    "url": "/reactagenda/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d911996e2f8324a224d",
+    "url": "/reactagenda/static/js/2.d64ac278.chunk.js"
+  },
+  {
+    "revision": "d44e0cfa1f51b25079454bd515ac8f9b",
+    "url": "/reactagenda/static/js/2.d64ac278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b4ae337b3f3cba56de",
+    "url": "/reactagenda/static/js/main.04090896.chunk.js"
+  },
+  {
+    "revision": "77b584de470988b08453",
+    "url": "/reactagenda/static/js/runtime-main.86a8fb89.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.debf325d88d336d9ae3fc00fca82543f.js"
+  "/reactagenda/precache-manifest.ae65af562c97208eed1472deeedfb115.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactagenda/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
